f7a9ec2620cfbb5d4ff5a2d89cffd95889b8689d
David Black <dblack@atlassian.com>
2013-09-20 14:00:50 +1000

    Switch to a markdown readme file.
    Signed-off-by: David Black <dblack@atlassian.com>

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='junit_xml_output',
 	author_email='dblack@atlassian.com',
 	url='https://bitbucket.org/db_atlass/python-junit-xml-output-module',
 	packages=find_packages(),
-	description=read('README'),
-	long_description=read('README'),
+	description=read('README.md'),
+	long_description=read('README.md'),
 	version = "0.0.1"
 	)
 
