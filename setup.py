710e93e9ba0f1be4540c449ba6621a3f1cd9d37f
David Black <dblack@atlassian.com>
2020-12-07 09:50:19 +1100

    Sem-Ver: bugfix Move classifiers and license information to setup.cfg
    Signed-off-by: David Black <dblack@atlassian.com>

--- setup.py
+++ setup.py
@@ -7,30 +7,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='junit-xml-output',
-      author='David Black',
+setup(author='David Black',
       author_email='dblack@atlassian.com',
       url='https://bitbucket.org/db_atlass/python-junit-xml-output-module',
       packages=find_packages(),
       description=read('README.txt'),
       long_description=read('README.txt'),
-      license="MIT",
       version=__import__('junit_xml_output').__version__,
       test_suite='junit_xml_output.test',
       platforms=['any'],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-      ],
       )
